040d24872ab87375e70d6d0ae473d6a107a1f438
Claudio Maia <prestador-claudio.maia@n5x.com.br>
2024-06-26 12:52:51 -0300

    feat(): adding crud to apolice controller

--- src/server/controllers/apolice.controller.ts
+++ src/server/controllers/apolice.controller.ts
@@ -54,3 +54,75 @@ export async function GET(req: NextRequest) {
     return NextResponse.json({ error: "Error to get data" }, { status: 500 });
   }
 }
+
+export async function PUT(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+
+    const apoliceId = typeof id === "string" ? parseInt(id, 10) : undefined;
+    const body = await req.json();
+    const { numero, valorPremio, segurado, coberturas } = body;
+
+    if (!apoliceId || !numero || !valorPremio || !segurado || !coberturas) {
+      return NextResponse.json({ error: "Missing required fields" });
+    }
+
+    const updatedApolice = await prisma.apolice.update({
+      where: { id: apoliceId },
+      data: {
+        numero,
+        valorPremio,
+        segurado: {
+          update: segurado,
+        },
+        coberturas: {
+          deleteMany: {},
+          createMany: {
+            data: coberturas,
+          },
+        },
+      },
+      include: {
+        segurado: true,
+        coberturas: true,
+      },
+    });
+
+    return NextResponse.json(updatedApolice, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Unable to update apolice" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+export async function DELETE(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url);
+    const id = searchParams.get("id");
+    const apoliceId = typeof id === "string" ? parseInt(id, 10) : undefined;
+
+    if (!apoliceId) {
+      return NextResponse.json({ error: "Missing apolice ID" });
+    }
+
+    const deletedApolice = await prisma.apolice.delete({
+      where: { id: apoliceId },
+    });
+
+    return NextResponse.json(deletedApolice, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Unable to delete apolice" },
+      { status: 500 }
+    );
+  } finally {
+    await prisma.$disconnect();
+  }
+}
