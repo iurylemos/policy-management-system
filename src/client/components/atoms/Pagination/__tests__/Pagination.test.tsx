7eeedba61fdc87ea9638c53f20efc33bc6d84939
Claudio Maia <prestador-claudio.maia@n5x.com.br>
2024-06-27 23:18:03 -0300

    feat(): adding test to pagination component

---
+++ src/client/components/atoms/Pagination/__tests__/Pagination.test.tsx
@@ -0,0 +1,71 @@
+import { expect, describe, it, vi, afterEach } from "vitest";
+import {
+  fireEvent,
+  render,
+  screen,
+  waitFor,
+  cleanup,
+} from "@testing-library/react";
+import PaginationPage from "@/src/client/components/atoms/Pagination";
+
+describe("PaginationPage", () => {
+  afterEach(() => {
+    cleanup();
+  });
+
+  it("should renders a button back page and dispatch props to handle page", async () => {
+    const functionPageChange = vi.fn();
+
+    const props = {
+      currentPage: 3,
+      totalPages: 10,
+      onPageChange: functionPageChange,
+    };
+
+    render(
+      <PaginationPage
+        currentPage={props.currentPage}
+        onPageChange={props.onPageChange}
+        totalPages={props.totalPages}
+      />
+    );
+
+    const button = screen.getByRole("button", { name: /Voltar/i });
+
+    expect(button).toBeDefined();
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(functionPageChange).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  it("should renders a button next page and dispatch props to handle page", async () => {
+    const functionPageChange = vi.fn();
+
+    const props = {
+      currentPage: 3,
+      totalPages: 10,
+      onPageChange: functionPageChange,
+    };
+
+    render(
+      <PaginationPage
+        currentPage={props.currentPage}
+        onPageChange={props.onPageChange}
+        totalPages={props.totalPages}
+      />
+    );
+
+    const button = screen.getByRole("button", { name: /Próxima/i });
+
+    expect(button).toBeDefined();
+
+    fireEvent.click(button);
+
+    await waitFor(() => {
+      expect(functionPageChange).toHaveBeenCalledTimes(1);
+    });
+  });
+});
